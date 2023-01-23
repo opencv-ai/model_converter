62d3c5502b64e05169fffa96143e157a95d27162
Alexey Perminov <alexey.perminov@xperience.ai>
2023-01-23 23:34:23 +0300

    build: Dependencies update + codestyle fixes

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,1 +1,16 @@
+# type: ignore[attr-defined]
+"""PyTorch model conversion to different formats"""
+
+from importlib import metadata as importlib_metadata
+
 from .converter import Converter
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
---
+++ converter/example.py
@@ -0,0 +1,20 @@
+"""TODO: remove this file with appropriate tests update."""
+"""Example of code."""
+
+
+def hello(name: str) -> str:
+    """Just an greetings example.
+
+    Args:
+        name (str): Name to greet.
+
+    Returns:
+        str: greeting message
+
+    Examples:
+        .. code:: python
+
+            >>> hello("Roman")
+            'Hello Roman!'
+    """
+    return f"Hello {name}!"
